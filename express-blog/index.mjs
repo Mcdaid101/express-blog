1a293a4927bf2545658a8ba92ef7cba6f416357b
Mcdaid101 <matthewmcdaid1@hotmail.com>
2023-11-24 22:02:35 +0000

    added tailwind css to project

--- express-blog/index.mjs
+++ express-blog/index.mjs
@@ -4,14 +4,11 @@ import bodyParser from "body-parser";
 const app = express();
 const port = 3000;
 
-
-
 app.use(express.static("public"));
 
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", (req, res) => {
-    //Step 1 - Make the get route work and render the index.ejs file.
     res.render("index.ejs");
   });
 
