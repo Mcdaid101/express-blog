6f5c78d79da60625de27f0b6cffac552be309f4a
Mcdaid101 <matthewmcdaid1@hotmail.com>
2023-11-24 23:16:52 +0000

    implemented blog posting functionality and displaying it on another page

--- express-blog/index.mjs
+++ express-blog/index.mjs
@@ -12,6 +12,16 @@ app.get("/", (req, res) => {
     res.render("index.ejs");
   });
 
+app.post("/submit", (req, res) => {
+  const blogTitle = req.body["title"];
+  const blogContent = req.body["content"];
+  console.log(blogContent, blogTitle);
+  res.render("post.ejs", {
+    title: blogTitle,
+    content: blogContent
+  })
+});
+
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
   });
