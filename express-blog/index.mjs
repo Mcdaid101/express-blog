1c847076a4032a26a1c5e73ff958000db2a4ce03
Mcdaid101 <matthewmcdaid1@hotmail.com>
2023-11-26 20:41:17 +0000

    added a blogs section where posted blogs display

--- express-blog/index.mjs
+++ express-blog/index.mjs
@@ -8,19 +8,40 @@ app.use(express.static("public"));
 
 app.use(bodyParser.urlencoded({extended: true}));
 
+let blogPosts = [];
+
+// renders home page
 app.get("/", (req, res) => {
     res.render("index.ejs");
   });
 
+//renders about page 
+app.get("/about", (req, res) => {
+  res.render("about.ejs");
+});
+
+// renders the blogs from the blogPosts array
+app.get("/blogs", (req, res) => {
+  res.render("blogs.ejs", {blogPosts});
+});
+
+//submits the blog from form and adds it to the blogPosts array
 app.post("/submit", (req, res) => {
   const blogTitle = req.body["title"];
   const blogContent = req.body["content"];
-  console.log(blogContent, blogTitle);
-  res.render("post.ejs", {
+  //console.log(blogContent, blogTitle);
+
+  const newPost = {
     title: blogTitle,
-    content: blogContent
-  })
+    content: blogContent,
+  };
+
+  blogPosts.push(newPost);
+
+  res.render("post.ejs", newPost);
+  console.log(newPost);
 });
+
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
